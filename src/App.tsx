cebacc3a4a2b16ad67bd118d1ef5412b9a3dddb5
João Cláudio <jca.olivera91@gmail.com>
2021-06-06 08:57:57 -0300

    chapter 02 finalized

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Dashboard } from "./components/Dashboard";
+import { useState } from 'react'
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionProvider } from "./hooks/useTransactions";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionProvider>
+
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionProvider>
+  )
+}
